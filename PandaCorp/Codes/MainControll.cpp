099ecebd9333942d84adc5a4ecd6da9aa1d420ea
Masaya_Kobayashi <kobayashi03@hotmail.co.jp>
2017-11-27 17:25:59 +0900

    [add]白桃君の入力管理処理を実装

--- PandaCorp/Codes/MainControll.cpp
+++ PandaCorp/Codes/MainControll.cpp
@@ -2,6 +2,7 @@
 
 #include "DxLib.h"
 #include "FPSManager.h"
+#include "InputManager.h"
 
 #include "GameScene.h"
 
@@ -24,6 +25,8 @@ void MainControll::doMainLoop(){
 
 	while(ScreenFlip() == 0 && ProcessMessage() != -1 && ClearDrawScreen() == 0){
 
+		InputManager::getInstance().update();	// プレイヤーの入力を更新
+
 		FPSManager::getInstance().adjustment();	// FPS調整
 		
 		if(mChangeSceneMan == nullptr){
@@ -41,8 +44,16 @@ void MainControll::doMainLoop(){
 		}
 
 #ifdef _DEBUG
+		int red = GetColor(255, 0, 0);
+
 		// 左上に現在のFPSを表示
-		DrawFormatString(0, 0, GetColor(255, 0, 0), "%d", FPSManager::getInstance().getFrameNumInSec());
+		DrawFormatString(0, 0, red, "%d", FPSManager::getInstance().getFrameNumInSec());
+
+		// 入力テスト
+		DrawFormatString(0, 30, red, "Left: %d", InputManager::getInstance().checkPushFrame(KEY_INPUT_LEFT));
+		DrawFormatString(0, 60, red, "Right: %d", InputManager::getInstance().checkPushFrame(KEY_INPUT_RIGHT));
+		DrawFormatString(0, 90, red, "Up: %d", InputManager::getInstance().checkPushFrame(KEY_INPUT_DOWN));
+		DrawFormatString(0, 120, red, "Down: %d", InputManager::getInstance().checkPushFrame(KEY_INPUT_UP));
 #endif
 	}
 
