8403e77d17837dd97b3d95c1687fdd92b030d20f
沼尻優作 <numa_11_03@yahoo.co.jp>
2017-12-11 21:37:42 +0900

    [add] 画面の大まかなレイアウト調整 　　　ハイスコアデータ(バイナリ形式)読み込みのテストコード導入

--- PandaCorp/Codes/StageSelect/StageSelectScene.cpp
+++ PandaCorp/Codes/StageSelect/StageSelectScene.cpp
@@ -4,11 +4,24 @@
 #include "DxLib.h"
 
 #define StageNum 6 //ステージ数
+#define PI    3.1415926535897932384626433832795f
 
 //グローバル変数宣言
-int stage_select = 0;
+int stage_select = 4;
 int step = 0;
 int select_decide = 0;
+int Fontstageselect; //ステージ選択で使っているフォント
+int Stageselect_Cursor;
+
+//セーブデータから読み込んだ値を保存する変数
+FILE *fp_hiscore;
+int hiscore[StageNum] = { 0, 0, 0, 0, 0, 0 };//ハイスコア保存用
+int stageappear = 1;//現在出現しているステージの数を保存
+
+//begin, end関数の代わりの処理をするための変数　それぞれ移植したら削除
+int _begin = 0;
+int _end = 0;
+
 
 scene_sig StageSelectScene::update() {
 	scene_sig rtn;
@@ -16,20 +29,81 @@ scene_sig StageSelectScene::update() {
 	int i;
 	int go_next = 0;
 
-	DrawFormatString(200, 600, GetColor(255, 255, 255), "stage_select: %d", stage_select);
+	//begin関数の代わり 移植したら削除
+	if (_begin == 0) {
 
+		//begin関数に入れる予定の処理
+		Fontstageselect = CreateFontToHandle(NULL, 40, 5, DX_FONTTYPE_NORMAL);
+		SetTransColor(255, 255, 255);
+		Stageselect_Cursor = LoadGraph("test_cursor.png");
 
-	//ステージ選択の描写処理
-	//※ステージ数変えるときに調整必須※
-	for (i = 0; i < StageNum; i++) {
-		if (i == stage_select) {
-			DrawBox(150 + i * 170, 80, 150 + i * 170 + 130, 260, GetColor(255, 255, 255), TRUE);
+		//以下データ読み込みのテスト用コード
+
+		//テスト　データ書き込み
+		/*
+		fopen_s(&fp_hiscore, "test_hi.dat", "wb");
+		if (fp_hiscore == NULL)
+		{
+			for (i = 0; i < StageNum; i++) {
+				hiscore[i] = 0;
+			}
 		}
 		else {
-			DrawBox(150 + i * 170, 80, 150 + i * 170 + 130, 260, GetColor(128, 128, 128), TRUE);
+			fwrite(&stageappear, sizeof(int), 1, fp_hiscore);
+			for (i = 0; i < StageNum; i++) {
+				fwrite(&hiscore[i], sizeof(int), 1, fp_hiscore);
+			}
 		}
-		DrawFormatString(150 + 65 + i * 170, 170, GetColor(0, 0, 0), "%d", i);
+		*/
+
+		//テスト　データ読み込み
+	    
+		fopen_s(&fp_hiscore, "test_hi.dat", "rb");
+
+		if (fp_hiscore == NULL)
+		{
+			for (i = 0; i < StageNum; i++) {
+				hiscore[i] = 0;
+			}
+		}
+		else {
+			fread(&stageappear, sizeof(int), 1, fp_hiscore);
+			for (i = 0; i < StageNum; i++) {
+				fread(&hiscore[i], sizeof(int), 1, fp_hiscore);
+			}
+		}
+		
+		fclose(fp_hiscore);
+
+		//↑ここまでテスト用コード
+
+		_begin = 1;
 	}
+
+	//ステージ選択の描写処理
+
+	//上部の処理
+	//※ステージ数変えるときに調整必須※
+	for (i = 1; i <= stageappear; i++) {
+		if (i == stage_select) {
+			DrawBox(150 + (i - 1) * 170, 80, 150 + (i - 1) * 170 + 130, 260, GetColor(255, 255, 255), TRUE);
+
+			//カーソルの描写
+			DrawGraph(150 + (i - 1) * 170 + 45, 30, Stageselect_Cursor, TRUE);
+		}
+		else {
+			DrawBox(150 + (i - 1) * 170, 80, 150 + (i - 1) * 170 + 130, 260, GetColor(128, 128, 128), TRUE);
+		}
+		DrawFormatStringToHandle(150 + 55 + (i - 1) * 170, 160, GetColor(0, 0, 0), Fontstageselect, "%d", i);
+	}
+
+	//下部の処理
+	DrawFormatStringToHandle(156, 520, GetColor(255, 255, 255), Fontstageselect, "Stage%d", stage_select);
+	DrawStringToHandle(994, 492, "ハイスコア", GetColor(255, 255, 255), Fontstageselect);
+	DrawFormatStringToHandle(1009, 566, GetColor(255, 255, 255), Fontstageselect, "%08d", hiscore[stage_select - 1]);
+
+	DrawBox(346, 320, 934, 656, GetColor(255, 255, 255), TRUE);
+	DrawStringToHandle(406, 420, "マップ全体像とか\nステージの情報を\n表示するところ", GetColor(0, 0, 0), Fontstageselect);
 
 	//ステージ選択に関する処理
 	if (step == 0) {
@@ -37,14 +111,14 @@ scene_sig StageSelectScene::update() {
 		//ステージ選択カーソル処理
 		//左キーの処理
 		if (InputManager::getInstance().checkPushFrame(KEY_INPUT_LEFT) == 1) {
-			if (stage_select > 0) {
+			if (stage_select > 1) {
 				stage_select--;
 			}
 		}
 
 		//右キーの処理
 		if (InputManager::getInstance().checkPushFrame(KEY_INPUT_RIGHT) == 1) {
-			if (stage_select < StageNum - 1) {
+			if (stage_select < stageappear) {
 				stage_select++;
 			}
 		}
@@ -60,12 +134,12 @@ scene_sig StageSelectScene::update() {
 		//確認ボタンの描写
 		DrawBox(246, 134, 1034, 586, GetColor(255, 255, 0), TRUE);
 		DrawBox(256, 144, 1024, 576, GetColor(255, 255, 255), TRUE);
-		DrawString(530, 204,"このステージで遊びますか？", GetColor(0, 0, 0));
-		DrawString(360, 404, "はい", GetColor(0, 0, 0));
-		DrawString(830, 404, "いいえ", GetColor(0, 0, 0));
+		DrawStringToHandle(380, 204, "このステージで遊びますか？", GetColor(0, 0, 0), Fontstageselect);
+		DrawStringToHandle(380, 484, "はい", GetColor(0, 0, 0), Fontstageselect);
+		DrawStringToHandle(830, 484, "いいえ", GetColor(0, 0, 0), Fontstageselect);
 
 		//カーソルの描写
-		DrawString(340 + select_decide*470, 404, "→", GetColor(0, 0, 0));
+		DrawRotaGraph2(360 + select_decide * 450, 504, 20, 20, 1.0, 2 * PI * 3 / 4, Stageselect_Cursor, TRUE);
 
 		//ステージ選択カーソル処理
 		//左キーの処理
@@ -98,13 +172,18 @@ scene_sig StageSelectScene::update() {
 		rtn.next = scene_id::NONE;
 	}
 	else {
-		rtn.next = scene_id::GAME;
-		rtn.meta = stage_select+1; // 1 ～ StageNum の値を返す
+		if (_end == 0) {
+			DeleteFontToHandle(Fontstageselect);
+			_end = 1;
+		}
+		else {
+			rtn.next = scene_id::GAME;
+			rtn.meta = stage_select; // 1 ～ StageNum の値を返す
+		}
 	}
 
 	return rtn;
 }
-
 
 // trueを返すまで beginの処理を継続する
 bool StageSelectScene::begin() {
